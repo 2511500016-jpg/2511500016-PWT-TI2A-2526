66061b14ed520e3d37d49e63153503a5cbaa9922
2511500016-PWD-TI1A-25261 <2511500016@mahasiswa.atmaluhur.ac.id>
2026-03-12 09:40:58 +0700

    index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
- session_start();
- require_once('config/koneksi.php');
- if (isset($_SESSION['username'])) {
- 
+  session_start();
+  require_once("config/koneksi.php");
+  if(isset($_SESSION['role'])) {
+    $role = $_SESSION['role'];
 ?>
 <!DOCTYPE html>
 <!--
@@ -64,101 +64,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div>
       </li>
 
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
+      
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -179,7 +85,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
         </div>
       </div>
 
@@ -195,61 +101,95 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div>
       </div>
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                MASTER
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
+ <!-- Sidebar Menu -->
+<nav class="mt-2">
+<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+<!-- Add icons to the links using the .nav-icon class
+with font-awesome or any other icon font library -->
+
+  <li class="nav-item menu-open">
+    <a href="#" class="nav-link active">
+      <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>
+  Master
+  <i class="right fas fa-angle-left"></i>
+    </p>
+    </a>
+
+<?php if ($role == 'admin') : ?>
+  <ul class="nav nav-treeview">
+     <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="far fa-circle nav-icon"></i>
+           <p>Guru</p>
+         </a>
+      </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                  <p>Siswa</p>
+                </a>
+            </li>
               <li class="nav-item">
                 <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>GURU</p>
+                <i class="far fa-circle nav-icon"></i>
+                <p>Mata Pelajaran</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>SISWA</p>
+                  <p>Kelas</p>
                 </a>
               </li>
-                            <li class="nav-item">
+            </ul>
+            <?php endif; ?>   
+
+            <?php if ($role == 'guru') : ?>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>MAPEL</p>
+                  <p>Profil</p>
                 </a>
               </li>
-                            <li class="nav-item">
+              <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>KELAS</p>
+                  <p>Kelas</p>
+                </a>
+              </li>
+            </ul>
+            <?php endif; ?>            
+
+            <?php if ($role == 'siswa') : ?>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Profil</p>
+                </a>
+              </li>
+            </ul>
+            <?php endif; ?> 
+
+          </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link active">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>Transaksi
+                    <i class="right fas fa-angle-left"></i>
+                  </p>
+                </a>
+                <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Jadwal</p>
                 </a>
               </li>
             </ul>
           </li>
-                    <li class="nav-item">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                TRANSAKSI
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>JADWAL</p>
-                </a>
-              </li>
           <li class="nav-item">
             <a href="logout.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
@@ -292,13 +232,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <div class="col-lg-12">
             <div class="card">
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Dashboard</h5>
 
                 <p class="card-text">
                   Selamat Datang Di Website Saya Mantap mweheheh
                 </p>
-
-
+                
               </div>
             </div>
 
@@ -306,6 +245,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </div>
           <!-- /.col-md-6 -->
 
+          </div>
           <!-- /.col-md-6 -->
         </div>
         <!-- /.row -->
@@ -348,8 +288,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </body>
 </html>
 <?php
-
-} else {
-    echo '<meta http-equiv="refresh" content="0;url=login.php">';
-}
+  } else {
+    echo"<meta http-equiv='refresh' content='0; url=login.php'>";
+  }
 ?>
--- login.php
+++ login.php
@@ -6,91 +6,108 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+<title>AdminLTE 3 | Log in</title>
+
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+<!-- iCheck bootstrap -->
+<link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+<!-- Theme style -->
+<link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 
 <body class="hold-transition login-page">
 
 <div class="login-box">
-  <div class="login-logo">
-    <a href="#"><b>Admin</b>LTE</a>
-  </div>
 
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+<div class="login-logo">
+<a href="#"><b>Admin</b>LTE</a>
+</div>
+<!-- /.login-logo -->
 
-      <form action="" method="post">
+<div class="card">
 
-        <div class="input-group mb-3">
-          <input type="text" name="username" class="form-control" placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
+<div class="card-body login-card-body">
 
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
+<p class="login-box-msg">Sign in to start your session</p>
 
-        <div class="row">
-          <div class="col-12">
-            <input type="submit" name="login" value="Login" class="btn btn-primary btn-block">
-          </div>
-        </div>
-
-      </form>
-
+<form action="#" method="post">
+  <div class="input-group mb-3">
+    <input type="text" name="username" id="username" class="form-control" placeholder="username">
+    <div class="input-group-append">
+      <div class="input-group-text">
+        <span class="fas fa-envelope"></span>
+      </div>
     </div>
   </div>
-</div>
 
+  <div class="input-group mb-3">
+    <input type="password" name="password" id="password" class="form-control" placeholder="password">
+    <div class="input-group-append">
+      <div class="input-group-text">
+        <span class="fas fa-lock"></span>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <!-- /.col -->
+    <div class="col-12">
+      <input type="submit" name="login" value="Login" class="btn btn-primary btn-block">
+    </div>
+    <!-- /.col -->
+  </div>
+</form>
+
+</div>
+<!-- /.login-card-body -->
+
+</div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 
 </body>
 </html>
 
 <?php
-if(isset($_POST['login'])){
-
+if (isset($_POST['username'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if(empty($username) || empty($password)){
-        echo "Data Tidak Boleh Kosong";
+    if (empty($username) || empty($password)) {
+        echo "Data Tidak Boleh kosong";
     } else {
+        $userquery = mysqli_fetch_array(mysqli_query($koneksi, 
+        "SELECT * FROM tabel_user WHERE username = '$username' AND password = '$password'"));
 
-        $query = mysqli_query($koneksi,"SELECT * FROM tabel_user WHERE username='$username' AND password='$password'");
-        $user = mysqli_fetch_array($query);
-
-        if($user){
-            $_SESSION['level'] = "admin";
+        if ($userquery) {
+            $_SESSION['role'] = $userquery['role'];
             $_SESSION['username'] = $username;
-
-            header("Location: index.php");
-            exit;
-        } else {
+            header("location:index.php");
+          } else {
             echo '<div class="alert alert-danger alert-dismissible">
-                    <button type="button" class="close" data-dismiss="alert">&times;</button>
-                    Login gagal! Username atau Password salah.
-                  </div>';
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+            Login gagal
+            </div>';
         }
+      }
     }
-}
 ?>
