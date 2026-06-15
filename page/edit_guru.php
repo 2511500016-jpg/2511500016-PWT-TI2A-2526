a88ba914136085b57acc3fdfa5f127bb4701a3cb
2511500016-PWD-TI1A-25261 <2511500016@mahasiswa.atmaluhur.ac.id>
2026-06-15 14:37:38 +0700

    edit_guru.php

--- page/edit_guru.php
+++ page/edit_guru.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0 text-dark">EDIT KELAS</h1>
+        <h1 class="m-0 text-dark">Edit Guru</h1>
       </div>
     </div>
   </div>
